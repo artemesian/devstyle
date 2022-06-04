4d8dc5dc8dde9c6cc57ecd450a2c736f2c5edbcd
artemesian <angelodiepe10@yahoo.com>
2022-06-04 10:02:37 +0100

    [API] connected with online backend

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const devURL = "http://localhost:50002";
-// const prodURL = "http://localhost:5002";
+// const devURL = "http://localhost:50002";
+const prodURL = "https://devstyle.herokuapp.com/";
 
 // Set config defaults when creating the instance
 const myAxios = axios.create({
-  baseURL: devURL,
+  baseURL: prodURL,
 });
 
 export default myAxios;
