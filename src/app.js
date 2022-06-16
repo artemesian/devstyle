0b44a33b3a363b79f6666f4243ae89400cd78894
artemesian <angelodiepe10@yahoo.com>
2022-06-16 03:55:39 +0100

    [FEATURE] Coming soon branch

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+// import { useState } from "react";
 import { Box, createTheme, ThemeProvider } from "@mui/material";
 import { Route, Routes, useLocation } from "react-router-dom";
 import ReactGA from "react-ga";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 
 //Pages
-import Home from "./pages/home.component.jsx";
-import Collection from "./pages/collection.component.jsx";
-import Ambassador from "./pages/ambassador.component.jsx";
-import About from "./pages/about.component.jsx";
-import Goodie from "./pages/goodie.component.jsx";
-import Checkout from "./pages/checkout.component.jsx";
-// import ComingSoon from "./pages/comingSoon.component.jsx";
+// import Home from "./pages/home.component.jsx";
+// import Collection from "./pages/collection.component.jsx";
+// import Ambassador from "./pages/ambassador.component.jsx";
+// import About from "./pages/about.component.jsx";
+// import Goodie from "./pages/goodie.component.jsx";
+// import Checkout from "./pages/checkout.component.jsx";
+import ComingSoon from "./pages/comingSoon.component.jsx";
 
 //Components
-import Nav from "./components/nav.component.jsx";
-import Customize from "./components/customize.component";
-import Newsletter from "./components/newsletter.component";
-import Footer from "./components/footer.component";
+// import Nav from "./components/nav.component.jsx";
+// import Customize from "./components/customize.component";
+// import Newsletter from "./components/newsletter.component";
+// import Footer from "./components/footer.component";
 import "./app.styles.scss";
 
-import { calculatePromoPrice } from "./utils/utils.script.js";
+// import { calculatePromoPrice } from "./utils/utils.script.js";
 // import myAxios from "./utils/axios.config.js";
 
 export const analyticsEventTracker = (category = "Unknown") => {
@@ -32,71 +33,71 @@ export const analyticsEventTracker = (category = "Unknown") => {
 
 const App = () => {
   ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
-  const [cart, setCart] = useState({});
+  // const [cart, setCart] = useState({});
   const location = useLocation();
 
-  const handleCart = (newCart) => {
-    localStorage.setItem("devStyle_cart", JSON.stringify(newCart));
-    setCart(newCart);
-  };
+  // const handleCart = (newCart) => {
+  //   localStorage.setItem("devStyle_cart", JSON.stringify(newCart));
+  //   setCart(newCart);
+  // };
 
-  const addToCart = (goodie) => {
-    let copyCart = { ...cart };
-    if (copyCart[goodie.cartID]) {
-      copyCart[goodie.cartID].quantity += goodie.quantity;
-    } else {
-      copyCart[goodie.cartID] = goodie;
-    }
-    toast.info(<div style={{ color: "#fff" }}> Dans le panier</div>, {
-      icon: "🗑️",
-      style: { textAlign: "center" },
-    });
-    handleCart(copyCart);
-  };
+  // const addToCart = (goodie) => {
+  //   let copyCart = { ...cart };
+  //   if (copyCart[goodie.cartID]) {
+  //     copyCart[goodie.cartID].quantity += goodie.quantity;
+  //   } else {
+  //     copyCart[goodie.cartID] = goodie;
+  //   }
+  //   toast.info(<div style={{ color: "#fff" }}> Dans le panier</div>, {
+  //     icon: "🗑️",
+  //     style: { textAlign: "center" },
+  //   });
+  //   handleCart(copyCart);
+  // };
 
-  const updateCart = (goodie, value) => {
-    let copyCart = { ...cart };
-    if (copyCart[goodie.cartID]) {
-      copyCart[goodie.cartID].quantity += value;
-      if (copyCart[goodie.cartID].quantity === 0) {
-        return deleteFromCart(goodie.cartID);
-      }
-    }
-    handleCart(copyCart);
-  };
+  // const updateCart = (goodie, value) => {
+  //   let copyCart = { ...cart };
+  //   if (copyCart[goodie.cartID]) {
+  //     copyCart[goodie.cartID].quantity += value;
+  //     if (copyCart[goodie.cartID].quantity === 0) {
+  //       return deleteFromCart(goodie.cartID);
+  //     }
+  //   }
+  //   handleCart(copyCart);
+  // };
 
-  const deleteFromCart = (id) => {
-    let copyCart = { ...cart };
-    delete copyCart[id];
-    handleCart(copyCart);
-  };
+  // const deleteFromCart = (id) => {
+  //   let copyCart = { ...cart };
+  //   delete copyCart[id];
+  //   handleCart(copyCart);
+  // };
 
-  const getTotalPrice = () => {
-    let total = Object.values(cart).reduce(
-      (acc, goodie, i) =>
-        (acc +=
-          goodie.quantity *
-          (goodie.inPromo
-            ? calculatePromoPrice(goodie.price, goodie.promoPercentage)
-            : goodie.price)),
-      0
-    );
+  // const getTotalPrice = () => {
+  //   let total = Object.values(cart).reduce(
+  //     (acc, goodie, i) =>
+  //       (acc +=
+  //         goodie.quantity *
+  //         (goodie.inPromo
+  //           ? calculatePromoPrice(goodie.price, goodie.promoPercentage)
+  //           : goodie.price)),
+  //     0
+  //   );
 
-    return total;
-  };
-  const getCartCount = () => {
-    let count = Object.entries(cart).length;
-    return count;
-  };
+  //   return total;
+  // };
+  // const getCartCount = () => {
+  //   let count = Object.entries(cart).length;
+  //   return count;
+  // };
 
-  useEffect(() => {
-    let cartFromLocalStorage = JSON.parse(
-      localStorage.getItem("devStyle_cart")
-    );
-    if (cartFromLocalStorage) {
-      setCart(cartFromLocalStorage);
-    }
-  }, []);
+  // useEffect(() => {
+  //   let cartFromLocalStorage = JSON.parse(
+  //     localStorage.getItem("devStyle_cart")
+  //   );
+  //   if (cartFromLocalStorage) {
+  //     setCart(cartFromLocalStorage);
+  //   }
+  // }, []);
 
   useEffect(() => {
     ReactGA.pageview(location.pathname + location.hash);
@@ -104,15 +105,15 @@ const App = () => {
 
   return (
     <Box>
-      <Nav
+      {/* <Nav
         cart={cart}
         deleteFromCart={deleteFromCart}
         getTotalPrice={getTotalPrice}
         getCartCount={getCartCount}
-      />
+      /> */}
       <Routes>
-        {/* <Route path="/" element={<ComingSoon />} /> */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ComingSoon />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/collection/:slug" element={<Collection />} />
         <Route path="/our-ambassadors" element={<Ambassador />} />
         <Route path="/about-us" element={<About />} />
@@ -131,11 +132,11 @@ const App = () => {
               updateCart={updateCart}
             />
           }
-        />
+        />*/}
       </Routes>
-      <Customize />
+      {/* <Customize />
       <Newsletter />
-      <Footer />
+      <Footer /> */}
     </Box>
   );
 };
--- src/pages/comingSoon.component.jsx
+++ src/pages/comingSoon.component.jsx
@@ -3,7 +3,9 @@ import ComingSoonVideo from "../assets/video/_DevStyle Coming Soon.mp4";
 import TwitterIcon from "../assets/icons/twitter.png";
 import FacebookIcon from "../assets/icons/facebook.png";
 import InstaIcon from "../assets/icons/insta.png";
+import TiktokWhiteIcon from "../assets/icons/tiktok-white.png";
 import WhatsappIcon from "../assets/icons/whatsapp.png";
+import { analyticsEventTracker } from "../app";
 
 function ComingSoon() {
   return (
@@ -24,6 +26,9 @@ function ComingSoon() {
       </video>
       <div style={{ position: "absolute", bottom: 100 }}>
         <a
+          onClick={() => {
+            analyticsEventTracker("SOCIAL-IN-COMING-SOON")("twitter");
+          }}
           target="_blank"
           rel="noopener noreferrer"
           href="https://twitter.com/_devstyle"
@@ -32,6 +37,9 @@ function ComingSoon() {
         </a>
         &nbsp; &nbsp;
         <a
+          onClick={() => {
+            analyticsEventTracker("SOCIAL-IN-COMING-SOON")("whatsapp");
+          }}
           target="_blank"
           rel="noopener noreferrer"
           href="https://api.whatsapp.com/send/?phone=695151114&text=Hello _DevStyle"
@@ -40,6 +48,20 @@ function ComingSoon() {
         </a>
         &nbsp; &nbsp;
         <a
+          onClick={() => {
+            analyticsEventTracker("SOCIAL-IN-COMING-SOON")("tiktok");
+          }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.tiktok.com/@_devstyle"
+        >
+          <img src={TiktokWhiteIcon} alt="tiktok icon" />
+        </a>
+        &nbsp; &nbsp;
+        <a
+          onClick={() => {
+            analyticsEventTracker("SOCIAL-IN-COMING-SOON")("facebook");
+          }}
           target="_blank"
           rel="noopener noreferrer"
           href="https://www.facebook.com/devstyl"
@@ -48,6 +70,9 @@ function ComingSoon() {
         </a>
         &nbsp; &nbsp;
         <a
+          onClick={() => {
+            analyticsEventTracker("SOCIAL-IN-COMING-SOON")("instagram");
+          }}
           target="_blank"
           rel="noopener noreferrer"
           href="https://www.instagram.com/_devstyle/"
